9dcb35a6f8abbd0cc3b29898a278e1416e66acdf
santa-sukitoku <santanaruse@gmail.com>
2019-12-10 00:05:57 +0900

    5-2まで

--- dogCat.py
+++ dogCat.py
@@ -1,3 +1,5 @@
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
 from keras import models
 from keras import layers
 from keras import optimizers
@@ -134,7 +136,159 @@ validation_generator = test_datagen.flow_from_directory(
     batch_size=20,
     class_mode='binary')
 
+# 無限ループとなるため，何らかのタイミングでbreakする必要がある
 for data_batch, labels_batch in train_generator:
     print('data batch shape:', data_batch.shape)
     print('labels batch shape:', labels_batch.shape)
     break
+
+# バッチジェネレータを使ってモデルを適合
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+# モデルを保存
+model.save('cats_and_dogs_small_1.h5')
+
+'''
+# 訓練時の損失値と成果率をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+'''
+
+# ImageDataGeneratorを通じてデータ拡張を設定
+datagen = ImageDataGenerator(rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,  # 等積変形をランダムに適用
+                             zoom_range=0.2,  # 図形の内側をランダムにズーム
+                             horizontal_flip=True,  # 画像の半分を水平方向にランダム反転
+                             fill_mode='nearest')  # 新たに作成されたピクセル埋めるための戦略
+
+# ランダムに生成された訓練画像の表示
+fnames = [os.path.join(train_cats_dir, fname)
+          for fname in os.listdir(train_cats_dir)]
+
+# 水増しする画像を選択
+img_path = fnames[3]
+
+# 画像を読み込み，サイズを変更
+img = image.load_img(img_path, target_size=(150, 150))
+
+# 形状が(150, 150, 3)のNumPy配列に変換
+x = image.img_to_array(img)
+
+# (1, 150, 150, 3)に変形
+x = x.reshape((1,) + x.shape)
+
+# ランダムに生成された画像のバッチを生成する
+# 無限ループとなるため，何らかのタイミングでbreakする必要がある
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
+
+# ドロップアウトが追加された新しいCNNを定義
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# データ口調ジェネレータを使ってCNNを訓練
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+# 検証データは水増しスべきでないことに注意
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # ターゲットディレクトリ
+    target_size=(150, 150),  # すべての画像のサイズを150x150に
+    batch_size=32,  # バッチサイズ
+    class_mode='binary')  # binary_crossentropyを使用するため二値のラベルが必要
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=100,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+# モデルを保存
+model.save('cats_and_dogs_small_1.h5')
+
+# 訓練時の損失値と成果率をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
