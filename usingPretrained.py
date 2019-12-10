129840836fbca84e51f98ee6fad51c74f22711d5
santa-sukitoku <santanaruse@gmail.com>
2019-12-10 21:56:25 +0900

    5.3.3まで

---
+++ usingPretrained.py
@@ -0,0 +1,244 @@
+import matplotlib.pyplot as plt
+from keras import layers
+from keras import optimizers
+from keras import models
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import os
+from keras.applications import VGG16
+
+# VGG16モデルの畳込みベースのインスタンス化
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
+# print(conv_base.summary())
+
+# 5.2.2項でsmallデータセットを格納したディレクトリへのパスがあることに注意
+base_dir = 'cats_and_dogs_small'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size: (i + 1) * batch_size] = features_batch
+        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            # ジェネレータはデータを無限ループで生成するため，画像を一通り処理したらbreakしなければいけない
+            break
+
+    return features, labels
+
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+# 全結合分類器の定義と訓練
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+history = model.fit(train_features, train_labels,
+                    epochs=30,
+                    batch_size=20,
+                    validation_data=(validation_features, validation_labels))
+
+'''
+# 結果をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()  # ドットは訓練データ，折れ線は検証データ
+'''
+
+# 畳み込みベースに全結合分類器を追加
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# print(model.summary())
+
+print('This is the number of trainable weights '
+      'before freezing the conv base:', len(model.trainable_weights))
+conv_base.trainable = False
+print('This is the number of trainable weights '
+      'after freezing the conv base:', len(model.trainable_weights))
+
+# 凍結された畳み込みベースを使ってモデル全体を訓練
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+# 検証データは水増しすべきではないことに注意
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # ターゲットディレクトリ
+    target_size=(150, 150),  # すべての画像を150x150に変更
+    batch_size=20,  # バッチサイズ
+    class_mode='binary')  # 損失関数としてbinary_crossentropyを使用するため二値ラベルが必要
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['acc'])
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50,
+                              verbose=2)
+
+'''
+# 結果をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()  # ドットは訓練データ，折れ線は検証データ
+'''
+
+# ファインチューニングのため，最初から特定の層までをすべて凍結
+conv_base.trainable = True
+
+set_trainable = False
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+# モデルのファインチューニング
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=100,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+model.save('cats_and_dogs_small_4.h5')
+
+# 結果をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+
+def smooth_curve(points, factor=0.8):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+        return smoothed_points
+
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+print('test acc:', test_acc)
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()  # ドットは訓練データ，折れ線は検証データ
