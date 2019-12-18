0d644205f9fa38d42c93b64e104b887196efbc53
santa-sukitoku <santanaruse@gmail.com>
2019-12-18 23:09:29 +0900

    5章終わり

---
+++ .ipynb_checkpoints/dogCat-checkpoint.py
@@ -0,0 +1,294 @@
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import os
+import shutil
+
+# 元のデータセットを展開したディレクトリのパス
+original_dataset_dir = 'kaggle_original_data'
+
+# より小さなデータセットを格納するディレクトリのパス
+base_dir = 'cats_and_dogs_small'
+os.mkdir(base_dir)
+
+# 訓練データセット，検証データセット，テストデータセットを配置するディレクトリ
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+# 訓練用の猫の画像を配置するディレクトリ
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+
+# 訓練用の犬の画像を配置するディレクトリ
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+
+# 検証用の猫の画像を配置するディレクトリ
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+
+# 検証用の犬の画像を配置するディレクトリ
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+
+# テスト用の猫の画像を配置するディレクトリ
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+
+# テスト用の犬の画像を配置するディレクトリ
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+
+# 最初の1000個の猫画像をtrain_cat_dirにコピー
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# 次の500個の猫画像をvalidation_cats_dirにコピー
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# 次の500個の猫画像をtest_cats_dirにコピー
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# 最初の1000個の犬画像をtrain_cat_dirにコピー
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# 次の500個の犬画像をvalidation_cats_dirにコピー
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# 次の500個の犬画像をtest_cats_dirにコピー
+fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))
+print('total training dog images:', len(os.listdir(train_dogs_dir)))
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+print('total test cat imgaes:', len(os.listdir(test_cats_dir)))
+print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+
+# 犬と猫を分類するための小さなCNNをインスタンス化
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+print(model.summary())
+
+# モデルのコンパイル
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# すべての画像を1/255ですけ―リウング
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # ターゲットディレクトリ
+    target_size=(150, 150),  # すべての画像のサイズを150x150に
+    batch_size=20,  # バッチサイズ
+    class_mode='binary')  # binary_crossentropyを使用するため二値のラベルが必要
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+# 無限ループとなるため，何らかのタイミングでbreakする必要がある
+for data_batch, labels_batch in train_generator:
+    print('data batch shape:', data_batch.shape)
+    print('labels batch shape:', labels_batch.shape)
+    break
+
+# バッチジェネレータを使ってモデルを適合
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+# モデルを保存
+model.save('cats_and_dogs_small_1.h5')
+
+'''
+# 訓練時の損失値と成果率をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+'''
+
+# ImageDataGeneratorを通じてデータ拡張を設定
+datagen = ImageDataGenerator(rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,  # 等積変形をランダムに適用
+                             zoom_range=0.2,  # 図形の内側をランダムにズーム
+                             horizontal_flip=True,  # 画像の半分を水平方向にランダム反転
+                             fill_mode='nearest')  # 新たに作成されたピクセル埋めるための戦略
+
+# ランダムに生成された訓練画像の表示
+fnames = [os.path.join(train_cats_dir, fname)
+          for fname in os.listdir(train_cats_dir)]
+
+# 水増しする画像を選択
+img_path = fnames[3]
+
+# 画像を読み込み，サイズを変更
+img = image.load_img(img_path, target_size=(150, 150))
+
+# 形状が(150, 150, 3)のNumPy配列に変換
+x = image.img_to_array(img)
+
+# (1, 150, 150, 3)に変形
+x = x.reshape((1,) + x.shape)
+
+# ランダムに生成された画像のバッチを生成する
+# 無限ループとなるため，何らかのタイミングでbreakする必要がある
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
+
+# ドロップアウトが追加された新しいCNNを定義
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# データ口調ジェネレータを使ってCNNを訓練
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+# 検証データは水増しスべきでないことに注意
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # ターゲットディレクトリ
+    target_size=(150, 150),  # すべての画像のサイズを150x150に
+    batch_size=32,  # バッチサイズ
+    class_mode='binary')  # binary_crossentropyを使用するため二値のラベルが必要
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=100,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+# モデルを保存
+model.save('cats_and_dogs_small_2.h5')
+
+# 訓練時の損失値と成果率をプロット
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
--- visualCNN.py
+++ visualCNN.py
@@ -1,3 +1,8 @@
+import cv2
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from keras.applications.vgg16 import VGG16
+from keras import backend as K
+from keras.applications import VGG16
 from keras import models
 import matplotlib.pyplot as plt
 import numpy as np
@@ -39,5 +44,231 @@ first_layer_activation = activations[0]
 print(first_layer_activation.shape)
 
 # 3番目のチャンネルを可視化
-plt.matshow(first_layer_activation[0, :, :, 3], cmap='viridis')
+# plt.matshow(first_layer_activation[0, :, :, 3], cmap='viridis')
+# plt.show()
+
+# 30番目のチャンネルを可視化
+# plt.matshow(first_layer_activation[0, :, :, 30], cmap='viridis')
+# plt.show()
+
+# 中間層の活性化ごとにすべてのチャンネルを可視化
+# プロットの一部として使用する層の名前
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+
+images_per_row = 16
+
+# 特徴マップを表示
+for layer_name, layer_activation in zip(layer_names, activations):
+    # 特徴マップに含まれている特徴量の数
+    n_features = layer_activation.shape[-1]
+
+    # 特徴マップの形状(1, size, size, n_features)
+    size = layer_activation.shape[1]
+
+    # この行列で活性化のチャンネルをタイル表示
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+    # 各フィルタを1つの大きな水平グリッドでタイル表示
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row]
+            # 特徴量の見た目を良くするための後処理
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size: (col + 1) * size,
+                         row * size: (row + 1) * size] = channel_image
+
+    # グリッドを表示
+    '''
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1],
+                        scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+    plt.show()
+    '''
+
+# フィルタを可視化するための損失テンソルの定義
+model = VGG16(weights='imagenet', include_top=False)
+
+layer_name = 'block3_conv1'
+filter_index = 0
+
+layer_output = model.get_layer(layer_name).output
+loss = K.mean(layer_output[:, :, :, filter_index])
+
+# gradientsの呼び出しはテンソル(kの場合はサイズ1)のリストを返す
+# このため，最初の要素(テンソル)だけを保持する
+grads = K.gradients(loss, model.input)[0]
+
+# 除算の前に1e-5を足すことで，0による除算を回避
+grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+
+# 入力値をNumPy配列で受け取り，出力値をNumPy配列で返す配列
+iterate = K.function([model.input], [loss, grads])
+
+# テストしてみる
+loss_value, grads_value = iterate([np.zeros((1, 150, 150, 3))])
+
+# 確率的勾配降下法を使って損失値を最大化
+input_img_data = np.random.random((1, 150, 150, 3)) * 20 + 128.
+# 勾配上昇法を40ステップ実行
+step = 1.  # 各勾配の更新の大きさ
+for i in range(40):
+    # 損失値と勾配値を計算
+    loss_value, grads_value = iterate([input_img_data])
+    # 損失が最大になる方向に入力画像を調整
+    input_img_data += grads_value * step
+
+# テンソルを有効な画像に変換するユーティリティ関数
+
+
+def deprocess_image(x):
+    # テンソルを正規化 : 中心を0，標準偏差を0.1にする
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+
+    # [0, 1]でクリッピング
+    x += 0.5
+    x = np.clip(x, 0, 1)
+
+    # RGB配列に変換
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+# フィルタを活性化するための関数
+
+
+def generate_pattern(layer_name, filter_index, size=150):
+    # ターゲット層のn番目のフィルタの活性化を最大化する損失関数を構築
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:, :, :, filter_index])
+
+    # この損失関数を使って入力画像の勾配を計算
+    grads = K.gradients(loss, model.input)[0]
+
+    # 正規化トリック : 勾配を正規化
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+
+    # 入力画像に基づいて損失値と勾配値を返す関数
+    iterate = K.function([model.input], [loss, grads])
+
+    # 最初はノイズが含まれたグレースケール画像を試用
+    input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
+
+    # 勾配上昇法を40ステップ実行
+    step = 1.
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+
+    img = input_img_data[0]
+    return deprocess_image(img)
+
+
+'''
+plt.imshow(generate_pattern('block3_conv1', 0))
 plt.show()
+'''
+
+# 層の各フィルタの応答パターンで構成されたグリッドの生成
+layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1']
+for layer_name in layers:
+    size = 64
+    margin = 5
+
+    # 結果を格納する空(黒)の画像
+    results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3))
+    for i in range(8):  # resultsグリッドの行を順番に処理
+        for j in range(8):  # resultsグリッドの列を順番に処理
+            # layer_nameのフィルタi + (j * 8)のパターンを生成
+            filter_img = generate_pattern(layer_name, i + (j * 8), size=size)
+
+            # resultsグリッドの矩形(i, j)に結果を配置
+            horizontal_start = i * size + i * margin
+            horizontal_end = horizontal_start + size
+            vertical_start = j * size + j * margin
+            vertical_end = vertical_start + size
+            results[horizontal_start:horizontal_end,
+                    vertical_start: vertical_end, :] = filter_img
+
+# resultsグリッドを表示
+'''
+plt.figure(figsize=(20, 20))
+plt.imshow(results)
+plt.show()
+'''
+
+# 出力側に全結合分類器が含まれていることに注意
+# ここまでのケースでは，この分類機を削除している
+model = VGG16(weights='imagenet')
+
+# ターゲット画像へのローカルパス
+img_path = '/Users/fchollet/Downloads/creative_commons_elephant.jpg'
+# ターゲット画像を読み込む:imgはサイズが224×224のPIL画像
+img = image.load_img(img_path, target_size=(224, 224))
+# xは形状が(224, 224, 3)のfloat32型のNumPy配列
+x = image.img_to_array(img)
+# この配列をサイズが(1, 224, 224, 3)のバッチに変換するために次元を追加
+x = np.expand_dims(x, axis=0)
+# バッチの前処理(チャネルごとに色を正規化)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+
+print('Predicted:', decode_predictions(preds, top=3)[0])
+np.argmax(preds[0])
+
+# Grad-CAM アルゴリズムの設定
+# 予測ベクトルの「アフリカゾウ」エントリ
+african_elephant_output = model.output[:, 386]
+# VGG16の最後の畳み込み層であるblock5_conv3の出力特徴マップ
+last_conv_layer = model.get_layer('block5_conv3')
+# block5_conv3の出力特徴マップでの「アフリカゾウ」クラスの勾配
+grads = K.gradients(african_elephant_output, last_conv_layer.output)[0]
+# 形状が(512,)のベクトル:
+# 各エントリは特定の特徴マップチャネルの勾配の平均強度
+pooled_grads = K.mean(grads, axis=(0, 1, 2))
+# 2頭のアフリカゾウのサンプル画像に基づいて、pooled_gradsと
+# block5_conv3の出力特徴マップの値にアクセスするための関数
+iterate = K.function([model.input],
+                     [pooled_grads, last_conv_layer.output[0]])
+# これら2つの値をNumPy配列として取得
+pooled_grads_value, conv_layer_output_value = iterate([x])
+# 「アフリカゾウ」クラスに関する「このチャネルの重要度」を
+# 特徴マップ配列の各チャネルに掛ける
+for i in range(512):
+    conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+# 最終的な特徴マップのチャネルごとの平均値が
+# クラスの活性化のヒートマップ
+heatmap = np.mean(conv_layer_output_value, axis=-1)
+
+# ヒートマップの後処理
+heatmap = np.maximum(heatmap, 0)
+heatmap /= np.max(heatmap)
+plt.matshow(heatmap)
+
+# ヒートマップを元の画像にスーパーインポーズ
+# cv2を使って元の画像を読み込む
+img = cv2.imread(img_path)
+# 元の画像と同じサイズになるようにヒートマップのサイズを変更
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+# ヒートマップをRGBに変換
+heatmap = np.uint8(255 * heatmap)
+# ヒートマップを元の画像に適用
+heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+# 0.4はヒートマップの強度係数
+superimposed_img = heatmap * 0.4 + img
+# 画像をディスクに保存
+cv2.imwrite('elephant_cam.jpg', superimposed_img)
